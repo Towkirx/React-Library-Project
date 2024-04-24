9dcee646685b0421c91ab7bc23dbd9f9e0565038
Towkir <towkirmiah5@gmail.com>
2024-04-24 16:03:18 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Nav from "./components/Nav";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars)
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
