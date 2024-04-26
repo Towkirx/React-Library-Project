d632caf364b383346d952a1ddaa50f766d4c5c7e
Towkir <towkirmiah5@gmail.com>
2024-04-26 18:54:56 -0400

    all files updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import Nav from "./components/Nav";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo"
 
 library.add(faBars)
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
+      <Route path="/" exact component={Home} />
+      <Route path="/books" render={() => <Books books={books} />}/>
+      <Route path="/books/1" render={() => <BookInfo books={books} />} />
+      <Footer />
     </div>
+    </Router>
   );
 }
 
